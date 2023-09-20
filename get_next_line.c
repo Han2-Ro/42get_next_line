ce388aad52f40f62f80cd7cb8b158cb7f6e3a7ed
Hannes Erik Rother <hrother@c4r4p5.42vienna.com>
2023-09-20 16:11:03 +0200

    initial commit

---
+++ get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 15:21:19 by hrother           #+#    #+#             */
+/*   Updated: 2023/09/20 16:07:49 by hrother          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static int	x = 0;
+	char		*buffer;
+
+	buffer = malloc(BUFFER_SIZE * sizeof(char));
+	read(fd, buffer, BUFFER_SIZE);
+	return (NULL);
+}
---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 15:23:07 by hrother           #+#    #+#             */
+/*   Updated: 2023/09/20 16:00:12 by hrother          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFER_SIZE 42
+
+char	*get_next_line(int fd);
+
+#endif
